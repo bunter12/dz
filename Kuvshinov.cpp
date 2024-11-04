e51d406eca94381c1bda260a38e975080b2990a2
HOSOLOVEROM4K <151087930+HOSOLOVEROM4K@users.noreply.github.com>
2024-11-04 22:29:31 +0300

    реализация алгоритма перебор делителей

---
+++ Kuvshinov.cpp
@@ -0,0 +1,21 @@
+﻿#include <vector>
+#include "Kuvshinov.h"
+
+std::vector<int> findDivisors(int n) {
+    std::vector<int> divisors;
+
+    if (n <= 0) {
+        return divisors; // Возвращаем пустой вектор для некорректного ввода
+    }
+
+    for (int i = 1; i * i <= n; ++i) {
+        if (n % i == 0) {
+            divisors.push_back(i);
+            if (i != n / i) { // Избегаем добавления квадрата корня дважды
+                divisors.push_back(n / i);
+            }
+        }
+    }
+
+    return divisors;
+}
---
+++ Kuvshinov.h
@@ -0,0 +1,3 @@
+#include <vector>
+
+std::vector<int> findDivisors(int n);
