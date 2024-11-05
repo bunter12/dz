6e9e8cff8be7d6a33a5cc5c843d35fd68d281386
mouussssee <m.oussse@vk.com>
2024-11-05 20:31:33 +0300

    override function

--- Nedelko_group.cpp
+++ Nedelko_group.cpp
@@ -2,9 +2,10 @@
 #include <vector>
 #include "Nedelko_group.h"
 
-bool sieveOfEratosthenes(int n) {
+std::vector<int> sieveOfEratosthenes(int n) {
     std::vector<bool> isPrime(n + 1, true);
-
+    std::vector<int> primeNumber;
+    
     for (int p = 2; p * p <= n; p++) {
         if (isPrime[p] == true) {
             for (int i = p * p; i <= n; i += p)
@@ -12,6 +13,10 @@ bool sieveOfEratosthenes(int n) {
         }
     }
 
-    return isPrime.back();
-
+    for (int i = 2; i<=n; i++){
+        if(isPrime[i]){
+            primeNumber.push_back(i);
+        }
+    }
+    return primeNumber;
 }
