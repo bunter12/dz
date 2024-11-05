d713fc6cdeeba6f05a8dbb88f85b2415469675ec
KseMphony <xenia.bursak@yandex.com>
2024-11-05 20:26:00 +0300

    Create Bursak.cpp

---
+++ Bursak.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <vector>
+#include "Bursak.h"
+using namespace std;
+
+vector<int> Sundaram(int N) {
+    int newN = (N - 1) / 2;
+    vector<bool> prime_numbers(newN + 1, true);
+    
+    // Алгоритм Сундарама для нахождения простых чисел
+    for (int i = 1; i <= newN; ++i) {
+        for (int j = i; (i + j + 2 * i * j) <= newN; ++j) {
+            prime_numbers[i + j + 2 * i * j] = false;
+        }
+    }
+
+    // Сбор простых чисел в вектор
+    vector<int> primes;
+    if (N > 2) {
+        primes.push_back(2); // Число 2 — единственное чётное простое
+    }
+
+    for (int i = 1; i <= newN; ++i) {
+        if (prime_numbers[i]) {
+            primes.push_back(2 * i + 1);
+        }
+ 
+
+    return primes;
+}
